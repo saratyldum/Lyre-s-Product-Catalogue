1d14d35648006d78c10ca38dbaf4d9c3c5af69c9
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-03-24 13:35:51 +0100

    started fetching product information

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,9 +1,11 @@
 import navigation from "./modules/navigation.js";
 import homeStyle from "./modules/homeStyle.js";
 import tabs from "./modules/tabs.js";
-import productImage from "./modules/productImage.js"
+import productImage from "./modules/productImage.js";
+import productInformation from "./modules/productInformation.js";
 
 navigation();
 tabs();
 await productImage();
 homeStyle();
+await productInformation();
--- _app/js/modules/productInformation.js
+++ _app/js/modules/productInformation.js
@@ -1,5 +1,23 @@
 import { sanity } from "../sanity.js";
 
 export default async function productInformation() {
-	
+	const query = `*[_type == 'product' && slug.current == $slug] {
+		title, 
+		information,
+		flavourProfile,
+		flavours[] -> {
+		  flavourName,
+		  "flavourIcon": flavourIcon.asset->url,
+		},
+		enjoy,
+		tasteProfile,
+		instructions
+	 }`;
+
+	 const param = {
+		'slug': 'american-malt'
+	 }
+
+	const productInformation = await sanity.fetch(query, param);
+	console.log(productInformation);
 }
