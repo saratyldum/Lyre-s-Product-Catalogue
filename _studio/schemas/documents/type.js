671c354183fa14b037fe2dfd4ea3c0bc4fc311ab
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-04-04 11:30:25 +0200

    removed options from alcohol types

--- _studio/schemas/documents/type.js
+++ _studio/schemas/documents/type.js
@@ -7,25 +7,25 @@ export default {
 			name: 'type',
 			title: 'Type',
 			type: 'string',
-			options: {
-				list: [
-					{ title: 'Absinthe', value: 'absinthe' },
-					{ title: 'Amaretto', value: 'amaretto' },
-					{ title: 'Aperitivo', value: 'aperitivo' },
-					{ title: 'Bourbon', value: 'bourbon' },
-					{ title: 'Coffee liqueur', value: 'coffeeLiqueur' },
-					{ title: 'Gin', value: 'gin' },
-					{ title: 'Rum', value: 'rum' },
-					{ title: 'Spritz', value: 'spiritz' },
-					{ title: 'Tequila', value: 'tequila' },
-					{ title: 'Triple sec', value: 'tripleSec' },
-					{ title: 'Vermouth', value: 'vermouth' },
-					{ title: 'Whisky', value: 'whisky' },
-				],
-				layout: 'radio'
-			}
 		},
 	]
-
-
 }
+
+
+// options: {
+// 	list: [
+// 		{ title: 'Absinthe', value: 'absinthe' },
+// 		{ title: 'Amaretto', value: 'amaretto' },
+// 		{ title: 'Aperitivo', value: 'aperitivo' },
+// 		{ title: 'Bourbon', value: 'bourbon' },
+// 		{ title: 'Coffee liqueur', value: 'coffeeLiqueur' },
+// 		{ title: 'Gin', value: 'gin' },
+// 		{ title: 'Rum', value: 'rum' },
+// 		{ title: 'Spritz', value: 'spiritz' },
+// 		{ title: 'Tequila', value: 'tequila' },
+// 		{ title: 'Triple sec', value: 'tripleSec' },
+// 		{ title: 'Vermouth', value: 'vermouth' },
+// 		{ title: 'Whisky', value: 'whisky' },
+// 	],
+// 	layout: 'radio'
+// }
