a27e52fdab0a688ccb115d2639270760e486831a
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-04-04 11:56:44 +0200

    Added category and alcohol type (#3)

---
+++ _studio/schemas/documents/alcoholType.js
@@ -0,0 +1,12 @@
+export default {
+	name: 'alcoholType',
+	title: 'Alcohol Type',
+	type: 'document',
+	fields: [
+		{
+			name: 'type',
+			title: 'Type',
+			type: 'string',
+		},
+	]
+}
--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -1,9 +1,9 @@
 import product from "./documents/product.js"
 import flavour from "./documents/flavour.js"
-import type from "./documents/type.js"
+import alcoholType from "./documents/alcoholType.js"
 
 export default [
 	product,
 	flavour,
-	type,
+	alcoholType,
 ]
