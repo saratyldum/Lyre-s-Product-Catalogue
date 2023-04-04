42e2edbc2381056aaef4f44c0c0e78700f6d1c53
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-04 13:51:14 +0200

    fixed categories

--- _app/js/modules/productImage.js
+++ _app/js/modules/productImage.js
@@ -1,13 +1,46 @@
 import { sanity } from "../sanity.js";
+import homeStyle from "./homeStyle.js";
+
 
 export default async function productImage() {
-	const query = `*[_type == 'product'] {
-		'slug': slug.current,
-		'image': image.asset->url
-	 }`;
+	const categoryButtons = document.querySelectorAll('.product-category button');
+	let category;
+	let images;
+	let param;
+
+	categoryButtons.forEach(button => {
+		button.addEventListener('click', handleCategoryButtonClick)
+	})
 
 
-	const images = await sanity.fetch(query);
+	images = await fetchImages();
+
+	 async function handleCategoryButtonClick(e) {
+		images = await fetchImages(e, param);
+		createNavigationImageDOM();
+		renderHTML()
+	 }
+
+	 function findParameter(e) {
+		category === undefined ? category = 'spirit' : category = e.target.id;
+
+		return {
+				'category': category,
+			 }
+	 }
+
+	 async function fetchImages(e, category) {
+		const query = `*[_type == 'product' && category == $category] {
+			'slug': slug.current,
+			'image': image.asset->url
+		 }`;
+		 
+		 param = findParameter(e);
+
+		 images = await sanity.fetch(query, param);
+
+		 return images;
+	 }
 
 	function createNavigationImageDOM() {
 		const navigationList = document.createElement('ul');
@@ -36,10 +69,14 @@ export default async function productImage() {
 	function renderHTML() {
 		const navigation = document.querySelector('.nav');
 		const navigationList = createNavigationImageDOM();
+
+		navigation.innerHTML = '';
 		
 		if(navigation !== null) {
 			navigation.appendChild(navigationList)
 		}
+
+		homeStyle();
 	}
 	
 	renderHTML()
