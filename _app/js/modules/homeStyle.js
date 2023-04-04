9c0507c7cc26e1bf4bab95b21f37f727979bca50
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-04 15:20:10 +0200

    started individual styling for categories

--- _app/js/modules/homeStyle.js
+++ _app/js/modules/homeStyle.js
@@ -4,6 +4,7 @@ export default  function  homeStyle() {
 	const bottles = document.querySelectorAll('.navigation__image');
 	const body = document.querySelector('body');
 	const mainBottle = document.querySelector('#hero-img');
+	const mainImageContainer = document.querySelector('.main_image');
 
 	bottles.forEach(bottle => {
 		bottle.addEventListener('click', changeColor);
@@ -11,7 +12,6 @@ export default  function  homeStyle() {
 
 	async function changeColor(e) {
 		const target = e.target.dataset.id;
-		const image = document.querySelector('.img');
 
 		const query = `*[_type == 'product' && slug.current == '${target}'][0] {
 			"imageURL": image.asset->url,
@@ -28,6 +28,7 @@ export default  function  homeStyle() {
 		if(product.category === 'spirit') {
 			document.documentElement.style.setProperty('--clickMeButton', "url(../../assets/icons/clickMe-button-spirit.svg)")
 		} else if (product.category === 'premixed') {
+			mainImageContainer.style.height = '350px'
 			document.documentElement.style.setProperty('--clickMeButton', "url(../../assets/icons/clickMe-button-premixed.svg)")
 		} else if (product.category === 'sparklingWine') {
 			document.documentElement.style.setProperty('--clickMeButton', "url(../../assets/icons/clickMe-button-sparklingWine.svg)")
