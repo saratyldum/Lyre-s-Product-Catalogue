91da3b18ac2775fd568ea3b9442c35f057e835f4
saratyldum <98975412+saratyldum@users.noreply.github.com>
2023-04-04 16:20:34 +0200

    FIXED TAB DOUBLE CLICKING

--- _app/js/modules/tabs.js
+++ _app/js/modules/tabs.js
@@ -8,20 +8,12 @@ export default function tabs() {
 	})
 
 	function handleTabClick(e) {
-		// e.preventDefault()
-		const currentTab = e.target.dataset.id;
-		const currentContent = tabsContent.item(currentTab)
-		toggleContent(currentContent);
-	}
-
-	function toggleContent(content) {
-		tabOpen = !tabOpen;
-		//sjekk om tab clicked allerede er åpen først
-		if(!tabOpen) {
-			content.style.display = 'block'
-		} else {
+		tabsContent.forEach(content => {
 			content.style.display = 'none'
+		})
 
-		}
+		const currentTab = e.target.dataset.id;
+		const currentContent = tabsContent.item(currentTab);
+		currentContent.style.display = 'block'
 	}
 }
